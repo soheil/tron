e12a61c1d92a87e4492623a377b91a7878d8ff25
Soheil <ysoheil@gmail.com>
2025-01-24 17:26:10 -0800

    compile

--- _/entry.js
+++ _/entry.js
@@ -1,1 +1,13 @@
-fs.mkdirSync('/tmp/entered');
+if (fs.existsSync('_/main.js')) {
+  const scriptText = fs.readFileSync('_/main.js', 'utf-8');
+  eval(scriptText);
+} else {
+  const response = await fetch('https://raw.githubusercontent.com/soheil/tron/refs/heads/main/_/main.js', {
+      headers: {
+          'Cache-Control': 'no-cache',
+          'Pragma': 'no-cache'
+      }
+  });
+  const scriptText = await response.text();
+  eval(scriptText);
+}
