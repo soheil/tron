25cc23106149ee598c972112254a4a86c7d8136b
Soheil <ysoheil@gmail.com>
2025-01-24 17:57:31 -0800

    compile

--- _/main.js
+++ _/main.js
@@ -1,17 +1,21 @@
-import { BrowserWindow } from 'electron';
+try {
+  import { BrowserWindow } from 'electron';
 
-// Somewhere in your `main.ts` where you set up your Electron app
-function createWindow() {
-  const mainWindow = new BrowserWindow({
-     width: 800,
-     height: 600,
-     webPreferences: {
-        nodeIntegration: true,
-        contextIsolation: false
-     }
-  });
+  // Somewhere in your `main.ts` where you set up your Electron app
+  function createWindow() {
+    const mainWindow = new BrowserWindow({
+       width: 800,
+       height: 600,
+       webPreferences: {
+          nodeIntegration: true,
+          contextIsolation: false
+       }
+    });
 
-  mainWindow.loadFile('index.html');
+    mainWindow.loadFile('index.html');
+  }
+
+  createWindow()
+} catch (e) {
+  console.log(e, '<<<<<<<');
 }
-
-createWindow()
