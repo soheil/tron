5426f1c081618fe82497fab21bba7af9d88313a8
Soheil <ysoheil@gmail.com>
2025-01-24 19:22:46 -0800

    compile

--- _/main.js
+++ _/main.js
@@ -1,22 +1,23 @@
-try {
   console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>');
-  import { BrowserWindow } from 'electron';
+// try {
+//   console.log('>>>>>>>>>>>>>>>>>>>>>>>>>>');
+//   import { BrowserWindow } from 'electron';
 
-  // Somewhere in your `main.ts` where you set up your Electron app
-  function createWindow() {
-    const mainWindow = new BrowserWindow({
-       width: 800,
-       height: 600,
-       webPreferences: {
-          nodeIntegration: true,
-          contextIsolation: false
-       }
-    });
+//   // Somewhere in your `main.ts` where you set up your Electron app
+//   function createWindow() {
+//     const mainWindow = new BrowserWindow({
+//        width: 800,
+//        height: 600,
+//        webPreferences: {
+//           nodeIntegration: true,
+//           contextIsolation: false
+//        }
+//     });
 
-    mainWindow.loadFile('index.html');
-  }
+//     mainWindow.loadFile('index.html');
+//   }
 
-  createWindow()
-} catch (e) {
-  console.log(e, '<<<<<<<<<<<<<<<<<<<<');
-}
+//   createWindow()
+// } catch (e) {
+//   console.log(e, '<<<<<<<<<<<<<<<<<<<<');
+// }
