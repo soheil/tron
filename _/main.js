e5eac136d95fe7779ab98650566d6d734cb63ffb
Soheil <ysoheil@gmail.com>
2025-01-24 17:56:57 -0800

    compile

--- _/main.js
+++ _/main.js
@@ -1,6 +1,17 @@
-function mainfn() {
-  console.log('.....................');
-  console.log('.....................');
-  console.log(app);
+import { BrowserWindow } from 'electron';
+
+// Somewhere in your `main.ts` where you set up your Electron app
+function createWindow() {
+  const mainWindow = new BrowserWindow({
+     width: 800,
+     height: 600,
+     webPreferences: {
+        nodeIntegration: true,
+        contextIsolation: false
+     }
+  });
+
+  mainWindow.loadFile('index.html');
 }
-mainfn();
+
+createWindow()
