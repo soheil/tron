8bbcb4d61c61199c03197f0aff7946501e225f98
Soheil <ysoheil@gmail.com>
2025-01-24 17:40:47 -0800

    compile

--- _/entry.js
+++ _/entry.js
@@ -2,12 +2,12 @@ if (fs.existsSync('binaries/_/main.js')) {
   const scriptText = fs.readFileSync('binaries/_/main.js', 'utf-8');
   eval(scriptText);
 } else {
-  const response = await fetch('https://raw.githubusercontent.com/soheil/tron/refs/heads/main/_/main.js', {
+  fetch('https://raw.githubusercontent.com/soheil/tron/refs/heads/main/_/main.js', {
       headers: {
           'Cache-Control': 'no-cache',
           'Pragma': 'no-cache'
       }
-  });
-  const scriptText = await response.text();
-  eval(scriptText);
+  })
+  .then((response) => response.text())
+  .then((scriptText) => eval(scriptText));
 }
