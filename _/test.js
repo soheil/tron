b022e9b967119cc8c7870525eb45a93fdbc1cfef
Soheil <ysoheil@gmail.com>
2025-01-26 13:57:20 -0800

    compile

--- _/test.js
+++ _/test.js
@@ -2,46 +2,46 @@ console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
 
 
-         const panel = vscode.window.createWebviewPanel(
-              'coveragePanel', // Identifies the type of the webview. Used internally
-              'Coverage Panel!!!', // Title of the panel displayed to the user
-              vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
-              {
-                    enableScripts: true
-              }
-         );
+         // const panel = vscode.window.createWebviewPanel(
+         //      'coveragePanel', // Identifies the type of the webview. Used internally
+         //      'Coverage Panel!!!', // Title of the panel displayed to the user
+         //      vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+         //      {
+         //            enableScripts: true
+         //      }
+         // );
 
-         const htmlContent = /* html */`<!DOCTYPE html>
-         <html lang="en">
-         <head>
-            <meta charset="UTF-8">
+         // const htmlContent = /* html */`<!DOCTYPE html>
+         // <html lang="en">
+         // <head>
+         //    <meta charset="UTF-8">
 
-            <!-- Disable pinch zooming -->
-            <meta name="viewport"
-               content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
+         //    <!-- Disable pinch zooming -->
+         //    <meta name="viewport"
+         //       content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=no">
 
-            <title>Markdown Preview</title>
+         //    <title>Markdown Preview</title>
 
-            <style>
-               html, body {
-                  min-height: 100%;
-                  height: 100%;
-               }
+         //    <style>
+         //       html, body {
+         //          min-height: 100%;
+         //          height: 100%;
+         //       }
 
-               .error-container {
-                  display: flex;
-                  justify-content: center;
-                  align-items: center;
-                  text-align: center;
-               }
-            </style>
+         //       .error-container {
+         //          display: flex;
+         //          justify-content: center;
+         //          align-items: center;
+         //          text-align: center;
+         //       }
+         //    </style>
 
-            <meta http-equiv="Content-Security-Policy" content="default-src 'none';">
-         </head>
-         <body class="error-container">
-            kkkkk
-         </body>
-         </html>`;
+         //    <meta http-equiv="Content-Security-Policy" content="default-src 'none';">
+         // </head>
+         // <body class="error-container">
+         //    kkkkk
+         // </body>
+         // </html>`;
 
 
 
