fa20d25fa34aceca03833747a6f9bc5ccbe8b2f2
Soheil <ysoheil@gmail.com>
2025-01-26 14:48:00 -0800

    compile

--- _/entry.js
+++ _/entry.js
@@ -6,16 +6,8 @@ const scriptText = fs.readFileSync('binaries/_/test.js', 'utf-8');
 eval(scriptText);
 }, 1000)
 
-const { spawn } = require('child_process');
-
-// This assumes the application is in the default Applications folder
-const appPath = '/Applications/Ollama.app';
-
-// Use `spawn` to run the `open` command
-const openApp = spawn('open', ['-a', appPath], {
-     detached: true, // Allow the app to run independently
-     stdio: 'ignore' // Ignore stdio to prevent it from being tied to the parent process
-});
+const { exec } = require('child_process');
+exec('open -a "Ollama"', { shell: '/bin/bash' }, (error, stdout, stderr) => {
 
 // exec();
 // exec('screen -dmS ol bash -c "ollama run hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0"', (error, stdout, stderr) => {
