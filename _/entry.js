4b565cc0c0dcd234f748e3ca64b8960b2a5d3c35
Soheil <ysoheil@gmail.com>
2025-01-26 14:25:56 -0800

    compile

--- _/entry.js
+++ _/entry.js
@@ -7,4 +7,8 @@ eval(scriptText);
 }, 1000)
 
 
+const { exec } = require('child_process');
+spawn('ollama', ['run', 'hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0']);
+
+
 console.log('__________ /EXT _____________________');
