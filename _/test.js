9983231a4942988ff1a51f16eb8e9e4fc138e5a9
Soheil <ysoheil@gmail.com>
2025-01-26 14:04:41 -0800

    compile

--- _/test.js
+++ _/test.js
@@ -3,7 +3,7 @@ console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 const aa = async () => {
    // vscode.window.showInputBox({ value: "hi" })
    await document = vscode.workspace.openTextDocument('/Users/soheil/tron/binaries/_/test.js')
-   vscode.window.showTextDocument(document, vscode.ViewColumn.Two);
+   // vscode.window.showTextDocument(document, vscode.ViewColumn.Two);
 }
 
 aa();
