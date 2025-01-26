b5ef8cb0a58760aa65dddfdb4095932603ed591a
Soheil <ysoheil@gmail.com>
2025-01-26 15:54:30 -0800

    compile

--- _/test.js
+++ _/test.js
@@ -7,7 +7,7 @@ const aa = async () => {
    const document = await vscode.workspace.openTextDocument(f)
    vscode.window.showTextDocument(document, vscode.ViewColumn.Two);
 
-   const filePath = '/tmp/ran23'
+   const filePath = '/tmp/ran24'
    const ran = fs.existsSync(filePath)
    if (!ran) {
      fs.writeFileSync(filePath, '');
@@ -47,11 +47,10 @@ function open_ollama_if_installed() {
       return;
     }
 
-    run_in_terminal('echo -e "\n\n\n\n\n\n************ PLEASE KEEP THIS TERMINAL RUNNING ************\n"; type uvx || curl -LsSf https://astral.sh/uv/install.sh | sh; ollama run hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0', 'Ollama')
+    run_in_terminal('echo -e "\n\n************ PLEASE KEEP THIS TERMINAL RUNNING ************\n"; type uvx || curl -LsSf https://astral.sh/uv/install.sh | sh; ollama run hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0', 'Ollama')
 
-    exec("uvx --with llm-ollama llm -m 'hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0' how_to_install_uv_from_the_cli > /Users/soheil/chat/gpt/2025-01-26_12_48_27.md 2>&1", (error, stdout, stderr) => {
-        console.log(error, stdout, stderr);
-    })
+    // exec("uvx --with llm-ollama llm -m 'hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0' how_to_install_uv_from_the_cli > /Users/soheil/chat/gpt/2025-01-26_12_48_27.md 2>&1")
+    exec('say hi')
   });
 }
 
