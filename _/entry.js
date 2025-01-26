b9cb2e3987c961ed11398bc6daf254a6c2252913
Soheil <ysoheil@gmail.com>
2025-01-26 14:33:12 -0800

    compile

--- _/entry.js
+++ _/entry.js
@@ -8,8 +8,9 @@ eval(scriptText);
 
 
 const { exec } = require('child_process');
-exec('open Ollama');
-exec('ollama run hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0', (error, stdout, stderr) => {
+exec();
+// exec('ollama run hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0', (error, stdout, stderr) => {
+exec('open Ollama', (error, stdout, stderr) => {
      if (error) {
           console.error(`exec error: ${error}`);
           return;
