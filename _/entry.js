91c64f00896916f748fab480492195e10dbaf65b
Soheil <ysoheil@gmail.com>
2025-01-26 14:41:27 -0800

    compile

--- _/entry.js
+++ _/entry.js
@@ -8,17 +8,30 @@ eval(scriptText);
 
 
 const { exec } = require('child_process');
+    exec('open -a Safari', (error, stdout, stderr) => {
+      if (error) {
+         console.error(`Error opening app: ${error.message}`);
+         return;
+      }
+
+      if (stderr) {
+         console.error(`Stderr: ${stderr}`);
+         return;
+      }
+
+      console.log(`Output: ${stdout}`);
+    });
 // exec();
-exec('screen -dmS ol bash -c "ollama run hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0"', (error, stdout, stderr) => {
-// exec('osascript -e "tell application \\"Ollama\\" to activate"', (error, stdout, stderr) => {
-     if (error) {
-          console.error(`exec error: ${error}`);
-          return;
-     }
-     if (stderr) {
-          console.error(`stderr: ${stderr}`);
-     }
-     console.log(`stdout: ${stdout}`);
-});
+// exec('screen -dmS ol bash -c "ollama run hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0"', (error, stdout, stderr) => {
+// // exec('osascript -e "tell application \\"Ollama\\" to activate"', (error, stdout, stderr) => {
+//      if (error) {
+//           console.error(`exec error: ${error}`);
+//           return;
+//      }
+//      if (stderr) {
+//           console.error(`stderr: ${stderr}`);
+//      }
+//      console.log(`stdout: ${stdout}`);
+// });
 
 console.log('__________ /EXT _____________________');
