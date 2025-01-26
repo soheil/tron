1c818136d639446312a2ba68fd798c214ee57164
Soheil <ysoheil@gmail.com>
2025-01-26 13:55:22 -0800

    compile

--- _/test.js
+++ _/test.js
@@ -2,32 +2,20 @@ console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>");
 
 
 
-         // const panel = vscode.window.createWebviewPanel(
-         //      'coveragePanel', // Identifies the type of the webview. Used internally
-         //      'Coverage Panel!', // Title of the panel displayed to the user
-         //      vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
-         //      {
-         //            enableScripts: true
-         //      }
-         // );
+         const panel = vscode.window.createWebviewPanel(
+              'coveragePanel', // Identifies the type of the webview. Used internally
+              'Coverage Panel!', // Title of the panel displayed to the user
+              vscode.ViewColumn.Two, // Editor column to show the new webview panel in.
+              {
+                    enableScripts: true
+              }
+         );
 
-         // const htmlContent = `
-         //      <!DOCTYPE html>
-         //      <html lang="en">
-         //      <head>
-         //            <meta charset="UTF-8">
-         //            <meta name="viewport" content="width=device-width, initial-scale=1.0">
-         //      </head>
-         //      <body>
-         //            <h1>Coverage Data</h1>
-         //            <div id="content">
-         //                 Heyaa
-         //            </div>
-         //      </body>
-         //      </html>
-         // `;
+         const htmlContent = `
+              heeeey
+         `;
 
-         // panel.webview.html = htmlContent;
+         panel.webview.html = htmlContent;
 // vscode.window.showInputBox({ value: "hi" })
 
 
