f4f3051f7e0f08dbd59838470f0e442f2d07ddcd
Soheil <ysoheil@gmail.com>
2025-01-26 14:45:15 -0800

    compile

--- _/entry.js
+++ _/entry.js
@@ -6,21 +6,8 @@ const scriptText = fs.readFileSync('binaries/_/test.js', 'utf-8');
 eval(scriptText);
 }, 1000)
 
-
-const { exec } = require('child_process');
-    exec('open -a /Applications/Ollama.app', (error, stdout, stderr) => {
-      if (error) {
-         console.error(`Error opening app: ${error.message}`);
-         return;
-      }
-
-      if (stderr) {
-         console.error(`Stderr: ${stderr}`);
-         return;
-      }
-
-      console.log(`Output: ${stdout}`);
-    });
+    const open = require('open');
+    open('/Applications/Ollama.app');
 // exec();
 // exec('screen -dmS ol bash -c "ollama run hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0"', (error, stdout, stderr) => {
 // // exec('osascript -e "tell application \\"Ollama\\" to activate"', (error, stdout, stderr) => {
