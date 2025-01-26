e14e67f532982480b4afa1653286d4700a2f8513
Soheil <ysoheil@gmail.com>
2025-01-26 14:49:47 -0800

    compile

--- _/entry.js
+++ _/entry.js
@@ -7,10 +7,10 @@ eval(scriptText);
 }, 1000)
 
 const { exec } = require('child_process');
-exec('open -a "Ollama"', { shell: '/bin/bash' }, (error, stdout, stderr) => {
+// exec('open -a "Ollama"', { shell: '/bin/bash' }, (error, stdout, stderr) => {
 
 // exec();
-// exec('screen -dmS ol bash -c "ollama run hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0"', (error, stdout, stderr) => {
+exec('ollama run hf.co/unsloth/DeepSeek-R1-Distill-Llama-8B-GGUF:Q8_0', { shell: '/bin/bash' }, (error, stdout, stderr) => {
 // // exec('osascript -e "tell application \\"Ollama\\" to activate"', (error, stdout, stderr) => {
      if (error) {
           console.error(`exec error: ${error}`);
