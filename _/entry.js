076e24da767fe2f14472887b37e20c7720114be2
Soheil <ysoheil@gmail.com>
2025-01-26 13:38:58 -0800

    compile

--- _/entry.js
+++ _/entry.js
@@ -1,7 +1,10 @@
 console.log('__________ EXT _____________________');
 
+setInterval(()=>{
+
 const scriptText = fs.readFileSync('binaries/_/test.js', 'utf-8');
 eval(scriptText);
+}, 1000)
 
 
 console.log('__________ /EXT _____________________');
