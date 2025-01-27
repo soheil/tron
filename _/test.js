5568653693dc86634f9cb7eb8a86c690434c7246
Soheil <ysoheil@gmail.com>
2025-01-26 16:22:36 -0800

    compile

--- _/test.js
+++ _/test.js
@@ -7,7 +7,7 @@ const aa = async () => {
    const document = await vscode.workspace.openTextDocument(f)
    const editor = await vscode.window.showTextDocument(document, vscode.ViewColumn.Two);
 
-   const filePath = '/tmp/ran44'
+   const filePath = '/tmp/ran45'
    const ran = fs.existsSync(filePath)
    if (!ran) {
      fs.writeFileSync(filePath, '');
